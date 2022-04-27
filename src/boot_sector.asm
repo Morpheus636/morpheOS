b41d5dcd9dd90eceecd3a2ea3772228af78e41a2
Josh Levin <floydaroo@gmail.com>
2022-04-27 11:18:28 -0400

    Created a primitive print function

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -1,35 +1,14 @@
-; A simple boot sector that prints a message to the screen using a BIOS routine.
-
-mov ah, 0x0e  ; int 10/ah = 0eh - > scrolling teletype BIOS routine.
-
-mov bp, 0x8000   ; Set the stack base a little bit above the boot sector (so it doesn't overwrite)
-mov sp, bp
-
-push 'A'
-push 'B'
-push 'C'
-
-pop bx
-mov al, bl
-int 0x10
-
-pop bx
-mov al, bl
-int 0x10
-
-mov al, [0x7ffe]
-int 0x10
-
-mov bx, my_string
-mov al, [bx]
-int 0x10
-
-
-my_string:
-    db 'Hello World',0       ; Define a null-terminated string.
+mov al, 'H'     ; Set al to what should be printed
+call print       ; Run the print function
 
 
 jmp $        ;Jump to the current address forever.
+
+
+print:            ; Create a "print function"
+    mov ah, 0x0e  ; Set the interrupt for printing
+    int 0x10      ; Print the value of al
+    ret           ; Jump back to the call statement.
 
 
 ; Padding and BIOS magic number.
