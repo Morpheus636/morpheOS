82a0088a878f1fae7925d1b8585b952f027425ab
Josh Levin <floydaroo@gmail.com>
2022-04-27 11:21:22 -0400

    pusha and popa in print function

--- src/boot_sector.asm
+++ src/boot_sector.asm
@@ -6,8 +6,10 @@ jmp $        ;Jump to the current address forever.
 
 
 print:            ; Create a "print function"
+    pusha         ; Push all registers onto the stack.
     mov ah, 0x0e  ; Set the interrupt for printing
     int 0x10      ; Print the value of al
+    popa          ; Pop all registers back from the stack.
     ret           ; Jump back to the call statement.
 
 
