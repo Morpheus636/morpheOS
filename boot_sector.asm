a100e3bd5570846f37399a259efafe211037daa9
Josh Levin <floydaroo@gmail.com>
2022-04-26 20:40:41 -0400

    Fixed boot sector asm sytntax

--- boot_sector.asm
+++ boot_sector.asm
@@ -1,9 +1,9 @@
 loop:                        ; Define a label that we can jump back to.
+
     jmp loop                 ; Use a simple CPU instruction to jump back to the label.
 
 
-times 510-($-$$)             ; Tell the assembler to pad out the program with zeroes
+times 510-($-$$) db 0        ; Tell the assembler to pad out the program with zeroes
                              ; (everything after the instructions but before the magic number)
 
 dw 0xaa55                    ; Make the last two bytes form the magic number.
-
