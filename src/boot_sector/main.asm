79411cfb84c9e5cd8ec30c3e618c40d2643af849
Josh Levin <floydaroo@gmail.com>
2022-04-29 23:48:38 -0400

    Hello World Kernel

--- src/boot_sector/main.asm
+++ src/boot_sector/main.asm
@@ -1,17 +1,20 @@
-mov al, 'H'     ; Set al to what should be printed
-call print       ; Run the print function
+[org 0x7c00] ; Tell the assembler to offset addresses of labels
+mov bp, 0x8000  ; Set the base memory location for the stack
+
+mov bx, hello_msg           ; Set al to the address of hello_msg
+call print_str              ; Run the print function
 
 
 jmp $        ;Jump to the current address forever.
 
 
-; Padding and BIOS magic number.
-; DO NOT TOUCH
+; Data
+hello_msg: db 'Hello World',0
 
-
+; Imports
 %include "src/boot_sector/print.asm"
 
+; Padding and BIOS Magic Number
+; DO NOT TOUCH
 times 510-($-$$) db 0        ; Tell the assembler to pad out the program with zeroes
-                             ; (everything after the instructions but before the magic number)
-
 dw 0xaa55                    ; Make the last two bytes form the magic number.
