850d44a594f8fde0165e73c76183523f2218e97b
Josh Levin <floydaroo@gmail.com>
2022-04-29 23:48:11 -0400

    better documentation for print string func

--- src/boot_sector/print.asm
+++ src/boot_sector/print.asm
@@ -2,17 +2,16 @@ print_str: ; Function to print a null-terminated string held in a memory address
     pusha         ; Push all registers onto the stack.
     mov ah, 0x0e  ; Set the interrupt for printing
 
-    iterate:
+   print_str_iterate:
         mov al, [bx]    ; pull a character from the current memory location to register AL
         cmp al, 0       ; return if al is null
         je print_string_end
 
         call print_char ; print the character in al
-        add bx, 1      ; incriment the memory location to the next location TODO - FIXME
-        jmp iterate
+        add bx, 1       ; incriment the memory location to the next character
+        jmp print_str_iterate
 
     print_string_end:
-        call print_char
         popa  ; Return all registers to the state they were when the function was called
         ret           ; Jump back to the call statement
 
