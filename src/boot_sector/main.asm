5b82c7de69140ab470a28efc881fe39c813e672b
Josh Levin <morpheus636@morpheus636.com>
2022-07-08 18:52:54 -0400

    Documentation and fixed the print_hex func

--- src/boot_sector/main.asm
+++ src/boot_sector/main.asm
@@ -1,21 +1,31 @@
 [org 0x7c00] ; Tell the assembler to offset addresses of labels
 mov bp, 0x8000  ; Set the base memory location for the stack
 
-mov bx, hello_msg           ; Set al to the address of hello_msg
-call print_str              ; Run the print function
+; Print the hello messgae.
+mov bx, hello_msg
+call print_str
 
-jmp $        ;Jump to the current address forever.
+; Load the kernel
+; It should block until the kernel shuts down.
+call load_kernel
 
 
-; Data
+; Exit params
+mov bx, exit_msg
+call print_str
+jmp $
+
+
+; Variable Definitions
 hello_msg: db 'Why would you do this to yourself?',0
+exit_msg: db 'Exit',0
 
 ; Imports
 %include "src/boot_sector/print.asm"
-%include "src/boot_sector/disk.asm"
+%include "src/boot_sector/boot_loader.asm"
+        
 
-
-; Padding and BIOS Magic Number
 ; DO NOT TOUCH
-times 510-($-$$) db 0        ; Tell the assembler to pad out the program with zeroes
-dw 0xaa55                    ; Make the last two bytes form the magic number.
+; Pad out the boot sector with zeroes then addd the magic number
+times 510-($-$$) db 0
+dw 0xaa55
--- src/boot_sector/print.asm
+++ src/boot_sector/print.asm
@@ -1,68 +1,97 @@
-print_str: ; Function to print a null-terminated string held in a memory address in bx
+print_str:
+    ; Function to print a null-terminated string held in the memory location stored at bx
+    ; Args:
+    ; - BX: Memory location of null-terminated string
     pusha         ; Push all registers onto the stack.
 
-   print_str_iterate:
-        ; Caboose's Mov:
-        mov al, [bx]    ; pull a character from the current memory location to register AL
-        cmp al, 0       ; return if al is null
+    print_str_iterate:
+        ; Pull a character from memory (Caboose's Mov)
+        mov al, [bx]
+
+        ; Return if the character is null (null terminated string)
+        cmp al, 0
         je print_string_end
 
-        call print_char ; print the character in al
-        add bx, 1       ; incriment the memory location to the next character
+        ; Print the character
+        call print_char
+
+        ; Re-iterate starting at the next memory location.
+        add bx, 1
         jmp print_str_iterate
 
     print_string_end:
-        mov al, 0xa     ; Print a newline
+        ; Print a newline then a carriage return
+        mov al, 0xa
         call print_char
-        mov al, 0xd     ; Print a carriage return
+        mov al, 0xd
         call print_char
-        popa  ; Return all registers to the state they were when the function was called
-        ret           ; Jump back to the call statement
+ 
+        ; Cleanup and return
+        popa
+        ret
 
 
 
-print_hex: ; Function to print a hex value from DX as a string (not a character)
+print_hex:
+    ; Function to print a hex value from DX as a string (not a character)
     ; Credit to kthompson on GitHub for parts of this function.
+    ; Args:
+    ; - DX: 16-bits to print (4 digits of hex)
     pusha  ; Push all registers to the stack
-
-    mov cx, 4 ; Start the counter for the loop at 4 (4 digits in a hex string)
+    
+    ; Iterate 4 times
+    mov cx, 4
     print_hex_iterate:
-        dec cx          ; decrement the counter for the loop
-        
+        dec cx  ; decrement the counter for the loop
+       
+        ; Get 4 bits from DX
         mov ax, dx      ; copy dx to ax so we can mask it for the last chars
         shr dx, 4       ; shift dx 4 bits to the right
-        and ax, 0xf     ; mask ax to get the last 4 bits
+        and ax, 0xf     ; mask ax to get the last 4 bits 
 
+        ; Figure out the location to set the character in.
         mov bx, HEX_OUT ; set bx to the mem address of the output string
-        add bx, 2       ; skip the 0x at the beginning
-        add bx, cx      ; add the current counter (CX) to the address
-
-        cmp ax,0xa      ; check if ax has a letter or a number
-        jl set_letter   ; if it's a number, set the value
-        add byte [bx],7 ; for ASCII letter, add 7
-        jl set_letter   ; then set the value
+        add bx, 2       ; skip the 0x at the beginning of the address
+        add bx, cx      ; add the current counter to the address
+        
+        ; Encode the 4 bits as a character into the string
+        cmp ax,0xa
+        jl set_letter   ; if ax is a number, set it in the output string
+        add byte al, 0x27 ; if ax is a letter, add 0x27 for ASCII encoding before setting
+        jl set_letter
         
     set_letter:
-        add byte [bx], al   ; Add the value of the byte to the char at bx
+        ; Set the letter into the string to print
+        add al, 0x30    ; Encode the character in ASCII by adding 0x30
+        mov byte [bx], al ; Move the character into its position in the string
 
         cmp cx, 0
         je print_hex_end       ; If the counter is done, return.
         jmp print_hex_iterate  ; Else, loop again
 
     print_hex_end:
-        mov bx, HEX_OUT ; Pass the address of the output to print_string
-        call print_str  ; Print the output
-        popa   ; Return all registers to their original state
-        ret    ; Jump back to the call statement
+        ; Print the outputted string
+        mov bx, HEX_OUT
+        call print_str
+        ; Cleanup and return
+        popa
+        ret
 
-    ;Variable Definitions
+    ; Local Variables
     HEX_OUT: db '0x0000',0
 
 
 
-print_char: ; Function to print the character in al
-    pusha         ; Push all registers to the stack
-    mov ah, 0x0e  ; Set the interrupt for BIOS tele-type
-    int 0x10      ; Call the interrupt to print
-    popa          ; Return all registers to their original state
-    ret           ; Jump back to the call statement
+print_char:
+    ; Function to print a single character from al
+    ; Args: 
+    ; - AL: Single character to print
+    pusha
+    
+    mov ah, 0x0e  ; Setup 0x10 interrupt for scrolling tele-type
+    int 0x10      ; Interrupt 0x10 to print the value of al. 
+
+    ; Cleanup and return
+    popa
+    ret
+
