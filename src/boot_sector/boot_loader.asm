1f9a766517bd73db1aa352ba95665aca9a49ac1c
Josh Levin <morpheus636@morpheus636.com>
2022-07-08 23:41:58 -0400

    setup MBR

--- src/boot_sector/boot_loader.asm
+++ src/boot_sector/boot_loader.asm
@@ -1,6 +1,13 @@
 load_kernel:
-    ; Loads and starts the kernel from disk.
+    ; Function to load and start the kernel from disk.
     pusha
+
     ; TODO - Write the bootloader
+    
+    ; Cleanup and return
     popa
     ret
+
+
+    ; Variable Definitions
+    BOOT_DRIVE: db 0
--- src/boot_sector/main.asm
+++ src/boot_sector/main.asm
@@ -1,31 +1,59 @@
-[org 0x7c00] ; Tell the assembler to offset addresses of labels
-mov bp, 0x8000  ; Set the base memory location for the stack
+[bits 16]
+[org 0x0600]
 
-; Print the hello messgae.
-mov bx, hello_msg
-call print_str
-
-; Load the kernel
-; It should block until the kernel shuts down.
-call load_kernel
+constants:
+    %define NEW_OFFSET 0x0600
+    %define OLD_OFFSET 0x7c00
 
 
-; Exit params
-mov bx, exit_msg
-call print_str
-jmp $
+pre_copy:
+    ; Disable Interrupts
+    cli
+
+    ; Zero segment registers
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+
+    ; Setup Stack
+    mov ss, ax
+    mov sp, 0x7c00
 
 
-; Variable Definitions
-hello_msg: db 'Why would you do this to yourself?',0
-exit_msg: db 'Exit',0
+copy_boot_sector:
+    mov di, 0x0600     ; Set destination location to 0x0600
+    mov si, 0x7c00     ; Set origin location to 0x7c00
+    mov cx, 512        ; Copy all 512 bytes of boot sector
+    rep movsb          ; Copy MBR one byte at a time
+    jmp 0:post_copy    ; Jump to the copied MBR's post_copy code
 
-; Imports
+
+post_copy:
+    ; Re-Enable Interrupts
+    sti
+
+    ; Print the hello messgae.
+    mov bx, HELLO_MSG
+    call print_str
+
+    ; Load the kernel
+    ; It should block until the kernel shuts down.
+    call load_kernel
+
+
+exit:
+    mov bx, EXIT_MSG
+    call print_str
+    jmp $
+
+; String Constants
+HELLO_MSG: db 'Why would you do this to yourself',0
+EXIT_MSG: db 'Exit',0
+
+;imports
 %include "src/boot_sector/print.asm"
 %include "src/boot_sector/boot_loader.asm"
-        
-
-; DO NOT TOUCH
-; Pad out the boot sector with zeroes then addd the magic number
-times 510-($-$$) db 0
-dw 0xaa55
+; THIS IMPORT MUST BE THE END OF THE FILE
+%include "src/boot_sector/partition_table.asm"
